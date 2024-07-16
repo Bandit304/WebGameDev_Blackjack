37a2614d72217bb4f43db173078d619f2093d0fc
ijkramer <kramerij@mail.uc.edu>
2024-07-16 11:48:50 -0400

    basic layout added, need way to set bet

--- src/game.js
+++ src/game.js
@@ -13,7 +13,8 @@ const config = {
         arcade: {
             gravity: false,
         }
-    }
+    },
+    backgroundColor: '#133926'
 };
 
 const game = new Phaser.Game(config);
@@ -84,8 +85,93 @@ function create(){
         this.physics.world.bounds.height / 2, // y position
         'deck'
     );
-}
+
+    dealerText = this.add.text(
+        (this.physics.world.bounds.width / 6) * 4 + 20,
+        this.physics.world.bounds.height - 570,
+        'dealer: 0',
+        {
+            fontFamily: 'Monaco, Courier, monospace',
+            fontSize: '30px',
+            fill: '#fff'
+        }
+    );
+
+    playerText = this.add.text(
+        (this.physics.world.bounds.width / 8) - 20,
+        this.physics.world.bounds.height - 570,
+        'player: 0',
+        {
+            fontFamily: 'Monaco, Courier, monospace',
+            fontSize: '30px',
+            fill: '#fff'
+        }
+    );
+
+    balanceBetText = this.add.text(
+        (this.physics.world.bounds.width / 6) * 2 + 60,
+        this.physics.world.bounds.height - 570,
+        'balance: 10 \nbet: 0',
+        {
+            fontFamily: 'Monaco, Courier, monospace',
+            fontSize: '20px',
+            fill: '#fff',
+            align: 'center'
+        }
+    );
+
+    //buttons
+     hitBtn = this.add.text(
+        (this.physics.world.bounds.width / 8) ,
+        this.physics.world.bounds.height - 30,
+        'HIT',
+        {
+            fontFamily: 'Monaco, Courier, monospace',
+            fontSize: '30px',
+            fill: '#fff',
+            align: 'center'
+        }
+    );
+    hitBtn.setInteractive()
+    .on('pointerover', () => hitBtn.setTint('0x0099cc'))
+    .on('pointerout', () => hitBtn.clearTint() )
+    .on('pointerdown', () => hitBtnPressed() )  //on button press
+    .on('pointerup', () => hitBtn.setTint('0x0099cc') )
+    ; 
+
+    standBtn = this.add.text(
+        (this.physics.world.bounds.width / 6) * 4 + 20,
+        this.physics.world.bounds.height - 30,
+        'STAND',
+        {
+            fontFamily: 'Monaco, Courier, monospace',
+            fontSize: '30px',
+            fill: '#fff',
+            align: 'center'
+        }
+    )
+    .setInteractive()
+    .on('pointerover', () => standBtn.setTint('0x0099cc'))
+    .on('pointerout', () => standBtn.clearTint() )
+    .on('pointerdown', () => standBtnPressed() )  //on button press
+    .on('pointerup', () => standBtn.setTint('0x0099cc') )
+    ;
+   
+   }
+
+  function standBtnPressed(){
+    standBtn.setTint('0x006800')
+    
+  }
+
+  function hitBtnPressed(){
+    hitBtn.setTint('0x006800')
+  }
+
+ 
+
 
 function update(){
+    
 
 }
