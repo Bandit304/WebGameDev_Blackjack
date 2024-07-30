1609d934d6e1b2bbc063203da37b45a07cba6192
Danny Murray <murraydp@mail.uc.edu>
2024-07-30 13:57:21 -0400

    Removed unnecessary preload function from BlackJackScene.js

--- src/Scenes/BlackJackScene.js
+++ src/Scenes/BlackJackScene.js
@@ -5,17 +5,17 @@ class BlackJackScene extends Phaser.Scene {
         super("BlackJackScene");
     }
 
-    preload() {        
-        this.load.image('deck', 'Assets/card-back1.png');
+    // preload() {        
+    //     this.load.image('deck', 'Assets/card-back1.png');
 
-        // Load cards
-        const suits = ['clubs', 'diamonds', 'hearts', 'spades'];
-        suits.forEach(suit => {
-            for (let i = 1; i <= 13; i++) {
-                this.load.image(`${suit}-${i}`, `Assets/card-${suit}-${i}.png`);
-            }
-        });
-    }
+    //     // Load cards
+    //     const suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+    //     suits.forEach(suit => {
+    //         for (let i = 1; i <= 13; i++) {
+    //             this.load.image(`${suit}-${i}`, `Assets/card-${suit}-${i}.png`);
+    //         }
+    //     });
+    // }
 
     // ===== Phaser.Scene Overrides =====
 
